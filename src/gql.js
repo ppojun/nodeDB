cb94ebd21138dd889deb5d1c48f448a9af158fe4
ppojun <120106@naver.com>
2022-03-31 20:33:18 +0900

    .

---
+++ src/gql.js
@@ -0,0 +1,71 @@
+// @ts-check
+
+const { ApolloServer, gql } = require('apollo-server')
+const { sequelize, User, City } = require('./sequelize')
+
+const typeDefs = gql`
+  type User {
+    id: Int!
+    name: String!
+    age: Int!
+    city: City
+  }
+  type City {
+    id: Int!
+    name: String!
+    users: [User]
+  }
+  type Query {
+    users: [User]
+  }
+`
+
+const resolvers = {
+  Query: {
+    users: async () => User.findAll(),
+  },
+  User: {
+    city: async (user) => {
+      City.findAll({
+        where: {
+          id: user.cityId,
+        },
+      })
+    },
+  },
+  City: {
+    users: async (city) => {
+      User.findAll({
+        where: {
+          cityId: city.id,
+        },
+      })
+    },
+  },
+}
+
+async function main() {
+  await sequelize.sync({ force: true })
+
+  const seoul = await City.build({
+    name: 'Seoul',
+  }).save()
+
+  await User.build({
+    age: 26,
+    name: 'Coco',
+    cityId: seoul.getDataValue('id'),
+  }).save()
+
+  await User.build({
+    age: 30,
+    name: 'Dodo',
+  }).save()
+
+  const server = new ApolloServer({ typeDefs, resolvers })
+  server.listen(5000).then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`)
+  })
+}
+
+main()
--- src/sequelize.js
+++ src/sequelize.js
@@ -1,79 +1,68 @@
 require('dotenv').config()
 const { Sequelize } = require('sequelize')
 
-async function main() {
-  const sequelize = new Sequelize({
-    username: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: process.env.DB_NAME,
-    port: Number(process.env.DB_PORT),
-    host: 'localhost',
-    dialect: 'mssql', //postgres'|'sqlite'|''|'mysql',
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
+const sequelize = new Sequelize({
+  username: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
+  host: 'localhost',
+  dialect: 'mssql', //postgres'|'sqlite'|''|'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+  // SQLite only
+  //storage: 'path/to/database.sqlite'
+})
+
+const User = sequelize.define(
+  'user',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    // SQLite only
-    //storage: 'path/to/database.sqlite'
-  })
-
-  const User = sequelize.define(
-    'user',
-    {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      age: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
     },
-    { timestamps: false }
-  )
-
-  const City = sequelize.define(
-    'city',
-    {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
+    age: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
     },
-    { timestamps: false }
-  )
+  },
+  { timestamps: false }
+)
 
-  User.belongsTo(City)
+const City = sequelize.define(
+  'city',
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  },
+  { timestamps: false }
+)
 
-  await sequelize.sync({
-    //alter: true,
-    force: true,
-  })
+User.belongsTo(City)
 
-  const newCity = City.build({
-    name: 'Seoul',
-  }).save()
+await sequelize.sync({
+  //alter: true,
+  force: true,
+})
 
-  const newUser = User.build({
-    name: 'Coco',
-    age: 24,
-    cityId: (await newCity).getDataValue('id'),
-  }).save()
-
-  await sequelize.authenticate()
-  await sequelize.close()
+module.exports = {
+  sequelize,
+  User,
+  City,
 }
-
-main()
